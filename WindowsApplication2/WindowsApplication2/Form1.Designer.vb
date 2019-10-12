5c43727cbf7aed08d66a1061585a182b6b86bccd
Averasi <56447978+Averasi@users.noreply.github.com>
2019-10-12 11:27:09 -0500

    Submission
    Our submission

---
+++ WindowsApplication2/WindowsApplication2/Form1.Designer.vb
@@ -0,0 +1,149 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Balance = New System.Windows.Forms.Label()
+        Me.Money = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Navy
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.SystemColors.Control
+        Me.Button1.Location = New System.Drawing.Point(12, 231)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(150, 125)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Send Money"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.Navy
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.ForeColor = System.Drawing.SystemColors.Control
+        Me.Button2.Location = New System.Drawing.Point(194, 273)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(155, 41)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Send"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Balance
+        '
+        Me.Balance.AutoSize = True
+        Me.Balance.Font = New System.Drawing.Font("Calibri", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Balance.ForeColor = System.Drawing.SystemColors.Control
+        Me.Balance.Location = New System.Drawing.Point(8, 134)
+        Me.Balance.Name = "Balance"
+        Me.Balance.Size = New System.Drawing.Size(161, 33)
+        Me.Balance.TabIndex = 5
+        Me.Balance.Text = "Your Balance:"
+        '
+        'Money
+        '
+        Me.Money.AutoSize = True
+        Me.Money.Font = New System.Drawing.Font("Calibri", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Money.ForeColor = System.Drawing.SystemColors.Control
+        Me.Money.Location = New System.Drawing.Point(234, 125)
+        Me.Money.Name = "Money"
+        Me.Money.Size = New System.Drawing.Size(158, 59)
+        Me.Money.TabIndex = 6
+        Me.Money.Text = "$50.00"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.LightCyan
+        Me.Label1.Location = New System.Drawing.Point(10, 10)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(72, 24)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.LightCyan
+        Me.Label2.Location = New System.Drawing.Point(10, 50)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(72, 24)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Label2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.LightCyan
+        Me.Label3.Location = New System.Drawing.Point(10, 90)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(72, 24)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Label3"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(194, 231)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(155, 20)
+        Me.TextBox2.TabIndex = 11
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.HotTrack
+        Me.ClientSize = New System.Drawing.Size(535, 368)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Money)
+        Me.Controls.Add(Me.Balance)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Balance As Label
+    Friend WithEvents Money As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox2 As TextBox
+End Class
---
+++ WindowsApplication2/WindowsApplication2/Form1.vb
@@ -0,0 +1,161 @@
+﻿Imports System.Speech.Recognition
+Imports System.Threading
+Imports System.Globalization
+Public Class Form1
+    Dim SAPI As Object
+    Dim Options As Integer = 0
+    Dim recog As New SpeechRecognizer
+    Dim gram As Grammar
+    Dim Line As String
+    Dim VoiceRec As Boolean = False
+
+    Public Event SpeechRecognized As _
+        EventHandler(Of SpeechRecognizedEventArgs)
+    Public Event SpeechRecognitionRejected As _
+        EventHandler(Of SpeechRecognitionRejectedEventArgs)
+    Dim RecognizableWords As String() = New String() {"Yes", "No"}
+    ' word recognised event
+    Public Sub recevent(ByVal sender As System.Object,
+            ByVal e As RecognitionEventArgs)
+        If (e.Result.Text = "Yes") Then
+            Select Case Options
+                Case 0
+                    Line = "Okay, the Online Wallet can be used to easily send money to and receive money from others almost instantly from any location! Would you like to see how it works?"
+                    SAPI.speak(Line, 1)
+                    Label1.Text = "Our Online Wallet makes money transactions easy."
+                    Label2.Visible = False
+                    Delay(4)
+                    Label2.Visible = True
+                    Label2.Text = "You don't have to go to the bank to make transactions"
+                    Delay(4)
+                    Label3.Visible = True
+                    Label3.Text = "Do you want to know how it works?"
+                    Options += 1
+
+                Case 1
+                    Line = "Great! we'll start with sending money. Let's pretend you have $50 in your wallet, and you want to send $20 to your friend John, but he's in another country. Using Paypal's online wallet, you can directly transfer $20 into his online wallet for him to spend at his leisure"
+                    SAPI.speak(Line, 1)
+                    Label1.Visible = False
+                    Label2.Visible = False
+                    Label3.Visible = False
+                    Delay(7)
+                    Label1.Text = ". . . Let's say you need to send $20 to your friend John."
+                    Label1.Visible = True
+                    Delay(3.5)
+                    Label2.Text = ". . . But he's in another country."
+                    Label2.Visible = True
+                    Delay(3)
+                    Label3.Text = "You can use Paypal's online wallet to do this instantly"
+                    Label3.Visible = True
+                    Delay(5)
+                    Options += 1
+                    Button1.Visible = True
+                    Balance.Visible = True
+                    Money.Visible = True
+                    Line = "Do you see the big, navy blue button labeled send money? Try clicking it now."
+                    SAPI.speak(Line, 1)
+                    Delay(5)
+                    Label2.Text = "Click the Send Money button to give John money."
+                    Label1.Visible = False
+                    Label3.Visible = False
+
+            End Select
+
+        ElseIf (e.Result.Text = "No") Then
+            Select Case Options
+                Case 0
+
+                    SAPI.speak("That's okay. I understand. Please have an excellent rest of your day. Goodbye!")
+                    Options += 1
+                    Me.Close()
+
+                Case 1
+
+            End Select
+        End If
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        SAPI = CreateObject("SAPI.spvoice")
+        Button1.Visible = False
+        Button2.Visible = False
+        Balance.Visible = False
+        TextBox2.Visible = False
+        Money.Visible = False
+        Label2.Visible = False
+        Label3.Visible = False
+
+    End Sub
+
+    Private Sub Form1_Shown(sender As Object, e As EventArgs) Handles MyBase.Shown
+        Line = "Welcome to Paypal's Interactive Agent, would you like to learn the benefits of Paypal's online wallet?"
+        SAPI.speak(Line, 1)
+
+        Dim words As New Choices(RecognizableWords)
+        gram = New Grammar(New GrammarBuilder(words))
+        recog.LoadGrammar(gram)
+        ' add handlers for the recognition events
+        AddHandler recog.SpeechRecognized, AddressOf Me.recevent
+        ' enable the recogniser
+        recog.Enabled = True
+        Label1.Text = "Welcome to PayPal's Interactive Agent!"
+        Delay(3)
+        Label2.Visible = True
+        Label2.Text = "Would you like to learn about our online wallet?"
+    End Sub
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Options = 2 Then
+            recog.Enabled = False
+            Options += 1
+            SAPI.speak("Great job! Now that you've selected send money, how much money do you want to send?", 1)
+            Button1.Visible = False
+            Label1.Visible = True
+            Label2.Visible = False
+            Label3.Visible = False
+            Label1.Text = ". . . Now that you've selected 'Send Money'"
+            Delay(3)
+            Label2.Visible = True
+            Label2.Text = ". . . How much money do you want to send?"
+            Delay(3)
+            Label3.Visible = True
+            Label3.Text = ". . . Click on the text box below and type in the number."
+            TextBox2.Visible = True
+            Button2.Visible = True
+
+        End If
+
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If Convert.ToInt32(TextBox2.Text) < 50 Then
+            Dim Total As Integer = Convert.ToInt32((TextBox2.Text))
+            Money.Text = "$" + (50 - Total).ToString
+            SAPI.speak("With just a click of a button, you just sent $20. Congratulations, you now know how to send money.", 1)
+            Button2.Visible = False
+            TextBox2.Visible = False
+            Label2.Visible = False
+            Label3.Visible = False
+            Label1.Text = "Congratulations! You did it!"
+        Else
+            SAPI.speak("The amount you entered is higher than your balance. Let's try again with a lower number.", 1)
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Sub Delay(ByVal dblSecs As Double)
+
+        Const OneSec As Double = 1.0# / (1440.0# * 60.0#)
+        Dim dblWaitTil As Date
+        Now.AddSeconds(OneSec)
+        dblWaitTil = Now.AddSeconds(OneSec).AddSeconds(dblSecs)
+        Do Until Now > dblWaitTil
+            Application.DoEvents()
+        Loop
+    End Sub
+End Class
